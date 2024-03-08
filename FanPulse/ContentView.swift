357b093b432be7ef4375d4810b4f85296790fec5
OtitoU <136706167+Odera19@users.noreply.github.com>
2024-03-08 00:35:09 -0600

    Initial Commit

---
+++ FanPulse/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FanPulse
+//
+//  Created by Otito Udedibor on 3/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FanPulse/FanPulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  FanPulseApp.swift
+//  FanPulse
+//
+//  Created by Otito Udedibor on 3/8/24.
+//
+
+import SwiftUI
+
+@main
+struct FanPulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
